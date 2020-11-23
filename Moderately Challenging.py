187ba81c182638be93f9bcfdf850efa5b7bde3d9
relydda20 <71363614+relydda20@users.noreply.github.com>
2020-11-23 14:22:06 +0700

    Update Moderately Challenging.py
    updated

--- Moderately Challenging.py
+++ Moderately Challenging.py
@@ -19,7 +19,7 @@ class Student(Person):
     def __init__(self, name, adress, numCourses = 0, courses = [], grades =[]):
         super().__init__(name, adress)
         self.__numCourses = int(numCourses)
-        self.__courses = str(courses)
+        self.__Courses = str(courses)
         self.__grades = int(grades)
     
     def addCourseGrade(self, courses, grades):
@@ -31,3 +31,29 @@ class Student(Person):
 
     def getAverageGrade(self):
         return sum(self.__grades) / len(self.__grades)
+
+    def toString(self):
+        return f"The name is {self.getName()}, The adress is {self.getAdress()}."
+
+class Teacher(Person):
+    def __init__(self, name, adress, numCourses = 0, courses =[]):
+        super().__init__(name, adress)
+        self.__numCourses = 0
+        self.__courses = []
+
+    def addCourse(self, course):
+        for courses in self.__courses:
+            if course == courses:
+                return False
+        self.__courses.append(course)
+        return True
+    
+    def removeCourse(self, course):
+        if course in self.__courses:
+            self.__courses.remove(course)
+            return True
+        else:
+            return False
+    
+    def toString(self):
+        return f"The name is {self.getName()}, The adress is {self.getAdress()}."
